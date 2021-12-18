31fb5b2760926839dab34f34dbc22ba64d127950
Mauricio <82846155+MauriManX@users.noreply.github.com>
2021-12-17 19:30:31 -0700

    Add files via upload

--- EVA3_1_ORDENAMIENTOS/src/eva3_1_ordenamientos/EVA3_1_ORDENAMIENTOS.java
+++ EVA3_1_ORDENAMIENTOS/src/eva3_1_ordenamientos/EVA3_1_ORDENAMIENTOS.java
@@ -20,13 +20,16 @@ public class EVA3_1_ORDENAMIENTOS {
         int [] datos = new int[10];
         llenar(datos);//Se llena el arreglo con valores aleatorios
         imprimir(datos);//Se imprime el arreglo
+        selectionSort(datos);
+        System.out.println("");
+        imprimir(datos);
         
         
     }
         //LLENADO del arreglo con valores aleatorios desde 0 a 99
         public static void llenar (int[] datos){
             for(int i=0; i<datos.length;i++){
-                datos[i] = (int)(Math.random());
+                datos[i] = (int)(Math.random()*100);
             }
         }
         //COPIAS DE ARREGLOS
@@ -34,6 +37,29 @@ public class EVA3_1_ORDENAMIENTOS {
         public static void imprimir (int[] datos){
             for(int i=0; i<datos.length;i++){
                 System.out.print("["+datos[i]+"]");
+            }
+        }
+        
+        //Eficiencia O(n^2)
+        public static void selectionSort(int [] datos){
+            for (int i =0; i<datos.length; i++){
+                int iMin = i; //Empieza el algoritmo. El mínimo es el primer elemento
+                for(int j = i+1; j< datos.length; j++){//Buscar la posicion del valor más bajo
+                    //Comprar valor[j] vs valor[min]
+                    if(datos[j]<datos[iMin]){//Se compara el actual contra el minimo actual
+                        iMin = j;
+                    }
+                }
+                //Intercambio
+                if(i != iMin){
+                //3 Pasos
+                //Se respalda un valor
+                int iTemp = datos[i];
+                //Se intercambia el valor
+                datos[i]=datos[iMin];
+                //Reponer el valor respaldado
+                datos[iMin] = iTemp;
+                }
             }
         }
 }
